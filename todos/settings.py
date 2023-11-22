bd1afe175e3ea0053f7629095430a48e68fb41c8
Jtrigo-corp <jtrigo.corp@gmail.com>
2023-11-22 01:15:17 -0300

    commit 8

--- todos/settings.py
+++ todos/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'todos.apps.TodosConfig',
-    'todoApp',
+    'todoApp.urls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
